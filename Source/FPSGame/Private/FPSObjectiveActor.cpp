d4cb63b000a71a7882bb31b3b433f2e6c11f3668
eyupalemdar <eyupalemdar@outlook.com>
2020-09-09 14:32:42 +0300

    Setup Actor Components

---
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -0,0 +1,32 @@
+// Copyright© 2020-2021 AlemdarLabs, All Rights Reserved
+
+#include "FPSObjectiveActor.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AFPSObjectiveActor::AFPSObjectiveActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSObjectiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSObjectiveActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
